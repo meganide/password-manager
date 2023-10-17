46a38dafffaab313fbcf93eff2e7b61e95e7191b
Meganide <renashassan@hotmail.com>
2023-10-17 15:27:36 +0200

    chore: setup aws amplify with cognito

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifypasswordgene77009ab": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
